05f26a29186ef918d65337cab94e291557dbdd68
Sarang Deshmukh <66946910+SarangDeshmukh7@users.noreply.github.com>
2020-11-16 15:48:16 +0530

    Update Drawing on Live Camera.py

--- Drawing on Live Camera.py
+++ Drawing on Live Camera.py
@@ -1,42 +1,6 @@
 import cv2
-import numpy as np
+import numpy
 
-cap = cv2.VideoCapture(0)
-
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-x = width//2
-y = height//2
-
-
-w = width//4
-h = height//4
-
-while True:
-
-    ret, frame = cap.read()
-
-
-    
-    cv2.rectangle(frame, (x, y), (x+w, y+h), color=(0,0,255),thickness= 4)
-
-
-    cv2.imshow('frame', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-
-
-
-cv2.destroyAllWindows()
-
-
-#########################################################################################################
-
-import cv2
 
 def draw_rectangle(event,x,y,flags,param):
 
