c08df73525e7e6a61560551b17d1ff934b6b393b
Sarang Deshmukh <66946910+SarangDeshmukh7@users.noreply.github.com>
2020-11-18 12:17:53 +0530

    Update Project_01.py

--- Project_01.py
+++ Project_01.py
@@ -1,51 +1,115 @@
-import cv2
-import numpy as np
-
-frameWidth = 640
-frameHeight = 480
-cap = cv2.VideoCapture(0)
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
-
-def empty(a):
-    pass
-
-cv2.namedWindow("HSV")
-cv2.resizeWindow("HSV",640,240)
-cv2.createTrackbar("HUE Min","HSV",0,179,empty)
-cv2.createTrackbar("HUE Max","HSV",179,179,empty)
-cv2.createTrackbar("SAT Min","HSV",0,255,empty)
-cv2.createTrackbar("SAT Max","HSV",255,255,empty)
-cv2.createTrackbar("VALUE Min","HSV",0,255,empty)
-cv2.createTrackbar("VALUE Max","HSV",255,255,empty)
-
-while True:
-
-    _, img = cap.read()
-    imgHsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-
-    h_min = cv2.getTrackbarPos("HUE Min","HSV")
-    h_max = cv2.getTrackbarPos("HUE Max", "HSV")
-    s_min = cv2.getTrackbarPos("SAT Min", "HSV")
-    s_max = cv2.getTrackbarPos("SAT Max", "HSV")
-    v_min = cv2.getTrackbarPos("VALUE Min", "HSV")
-    v_max = cv2.getTrackbarPos("VALUE Max", "HSV")
-    print(h_min)
-
-    lower = np.array([h_min,s_min,v_min])
-    upper = np.array([h_max,s_max,v_max])
-    mask = cv2.inRange(imgHsv,lower,upper)
-    result = cv2.bitwise_and(img,img, mask = mask)
-
-    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-    hStack = np.hstack([img,mask,result])
-    #cv2.imshow('Original', img)
-    #cv2.imshow('HSV Color Space', imgHsv)
-    #cv2.imshow('Mask', mask)
-   #cv2.imshow('Result', result)
-    cv2.imshow('Horizontal Stacking', hStack)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+import numpy as np 
+import cv2 
+  
+  
+# Capturing video through webcam 
+webcam = cv2.VideoCapture(0) 
+  
+# Start a while loop 
+while(1): 
+      
+    # Reading the video from the 
+    # webcam in image frames 
+    _, imageFrame = webcam.read() 
+  
+    # Convert the imageFrame in  
+    # BGR(RGB color space) to  
+    # HSV(hue-saturation-value) 
+    # color space 
+    hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV) 
+  
+    # Set range for red color and  
+    # define mask 
+    red_lower = np.array([136, 87, 111], np.uint8) 
+    red_upper = np.array([180, 255, 255], np.uint8) 
+    red_mask = cv2.inRange(hsvFrame, red_lower, red_upper) 
+  
+    # Set range for green color and  
+    # define mask 
+    green_lower = np.array([25, 52, 72], np.uint8) 
+    green_upper = np.array([102, 255, 255], np.uint8) 
+    green_mask = cv2.inRange(hsvFrame, green_lower, green_upper) 
+  
+    # Set range for blue color and 
+    # define mask 
+    blue_lower = np.array([94, 80, 2], np.uint8) 
+    blue_upper = np.array([120, 255, 255], np.uint8) 
+    blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper) 
+      
+    # Morphological Transform, Dilation 
+    # for each color and bitwise_and operator 
+    # between imageFrame and mask determines 
+    # to detect only that particular color 
+    kernal = np.ones((5, 5), "uint8") 
+      
+    # For red color 
+    red_mask = cv2.dilate(red_mask, kernal) 
+    res_red = cv2.bitwise_and(imageFrame, imageFrame,  
+                              mask = red_mask) 
+      
+    # For green color 
+    green_mask = cv2.dilate(green_mask, kernal) 
+    res_green = cv2.bitwise_and(imageFrame, imageFrame, 
+                                mask = green_mask) 
+      
+    # For blue color 
+    blue_mask = cv2.dilate(blue_mask, kernal) 
+    res_blue = cv2.bitwise_and(imageFrame, imageFrame, 
+                               mask = blue_mask) 
+   
+    # Creating contour to track red color 
+    contours, hierarchy = cv2.findContours(red_mask, 
+                                           cv2.RETR_TREE, 
+                                           cv2.CHAIN_APPROX_SIMPLE) 
+      
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 300): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            imageFrame = cv2.rectangle(imageFrame, (x, y),  
+                                       (x + w, y + h),  
+                                       (0, 0, 255), 2) 
+              
+            cv2.putText(imageFrame, "Red Colour", (x, y), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
+                        (0, 0, 255))     
+  
+    # Creating contour to track green color 
+    contours, hierarchy = cv2.findContours(green_mask, 
+                                           cv2.RETR_TREE, 
+                                           cv2.CHAIN_APPROX_SIMPLE) 
+      
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 300): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            imageFrame = cv2.rectangle(imageFrame, (x, y),  
+                                       (x + w, y + h), 
+                                       (0, 255, 0), 2) 
+              
+            cv2.putText(imageFrame, "Green Colour", (x, y), 
+                        cv2.FONT_HERSHEY_SIMPLEX,  
+                        1.0, (0, 255, 0)) 
+  
+    # Creating contour to track blue color 
+    contours, hierarchy = cv2.findContours(blue_mask, 
+                                           cv2.RETR_TREE, 
+                                           cv2.CHAIN_APPROX_SIMPLE) 
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 300): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            imageFrame = cv2.rectangle(imageFrame, (x, y), 
+                                       (x + w, y + h), 
+                                       (255, 0, 0), 2) 
+              
+            cv2.putText(imageFrame, "Blue Colour", (x, y), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 
+                        1.0, (255, 0, 0)) 
+              
+    # Program Termination 
+    cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame) 
+    if cv2.waitKey(10) & 0xFF == ord('q'): 
+        cap.release() 
+        cv2.destroyAllWindows() 
         break
-
-cap.release()
-cv2.destroyAllWindows()
